82e44fbb5d6166afa3ab2e324de894c7b7b0572d
Chris McHenry <cmchenry@aviatrix.com>
2023-07-05 19:04:33 -0400

    smartgroups for built-in internet and webgroup

--- demo-infra/smart_groups.tf
+++ demo-infra/smart_groups.tf
@@ -4,56 +4,54 @@ resource "aviatrix_smart_group" "private_networks" {
     match_expressions {
       cidr = "10.0.0.0/8"
     }
+    match_expressions {
+      cidr = "172.16.0.0/12"
+    }
   }
 }
 
-resource "aviatrix_smart_group" "internet" {
-  name = "PublicInternet"
+resource "aviatrix_smart_group" "aws_egress_vpc" {
+  name = "AWS-Egress-Demo-VPC"
   selector {
     match_expressions {
-      cidr = "1.0.0.0/8"
+      type = "vpc"
+      tags = {
+        Name = "egress-demo-vpc-1"
+      }
     }
-
-    match_expressions {
-      cidr = "2.0.0.0/7"
-    }
-
-    match_expressions {
-      cidr = "4.0.0.0/6"
-    }
-
-    match_expressions {
-      cidr = "8.0.0.0/7"
-    }
-
-    match_expressions {
-      cidr = "11.0.0.0/8"
-    }
-
-    match_expressions {
-      cidr = "12.0.0.0/6"
-    }
-
-    match_expressions {
-      cidr = "16.0.0.0/4"
-    }
-
-    match_expressions {
-      cidr = "32.0.0.0/3"
-    }
-
-    match_expressions {
-      cidr = "64.0.0.0/2"
-    }
-
-    match_expressions {
-      cidr = "128.0.0.0/2"
-    }
-
-    match_expressions {
-      cidr = "192.0.0.0/3"
-    }
-
   }
-
 }
+
+resource "aviatrix_smart_group" "aws_egress_vpc" {
+  name = "ThreatIQ-Blocklist"
+  selector {
+    match_expressions {
+      cidr = "172.16.0.0/12"
+    }
+  }
+}
+
+resource "aviatrix_web_group" "vpc1_allow" {
+  name = "vpc1_allow"
+  selector {
+    match_expressions {
+      snifilter = "portal.azure.com"
+    }
+    match_expressions {
+      snifilter = "*.amazon.com"
+    }
+    match_expressions {
+      snifilter = "*.google.com"
+    }
+  }
+}
+
+resource "aviatrix_web_group" "facebook" {
+  name = "facebook"
+  selector {
+    match_expressions {
+      snifilter = "*.facebook.com"
+    }
+  }
+}
+
