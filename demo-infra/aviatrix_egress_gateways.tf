cd3ae3cf590ca262498c364819eadc098e586d64
Chris McHenry <cmchenry@aviatrix.com>
2023-07-05 19:00:34 -0400

    enable 3+ AZ spoke with 7.1b

--- demo-infra/aviatrix_egress_gateways.tf
+++ demo-infra/aviatrix_egress_gateways.tf
@@ -1,6 +1,6 @@
-# Create an Aviatrix Spoke/Egress Gateways - Preferred Option for <=2 AZs in 7.0
+# Create a Aviatrix Spoke/Egress Gateways
 resource "aviatrix_spoke_gateway" "secure_egress_vpc1" {
-  count             = var.deploy_avx_egress_gateways && var.number_of_azs < 3 ? 1 : 0
+  count             = var.deploy_avx_egress_gateways ? 1 : 0
   cloud_type        = 1
   account_name      = var.aviatrix_aws_account_name
   gw_name           = "avx-gateway-vpc1"
@@ -9,6 +9,7 @@ resource "aviatrix_spoke_gateway" "secure_egress_vpc1" {
   gw_size           = var.avx_gateway_size
   subnet            = aws_subnet.public_vpc1[0].cidr_block
   single_ip_snat    = var.enable_nat_avx_egress_gateways
+  single_az_ha = true
   manage_ha_gateway = false
   depends_on = [
     aws_route_table_association.public_vpc1
@@ -16,26 +17,10 @@ resource "aviatrix_spoke_gateway" "secure_egress_vpc1" {
 }
 
 resource "aviatrix_spoke_ha_gateway" "secure_egress_vpc1_ha" {
-  count           = var.deploy_avx_egress_gateways && var.number_of_azs < 3 ? 1 : 0
+  count           = var.deploy_avx_egress_gateways ? var.number_of_azs - 1 : 0
   primary_gw_name = aviatrix_spoke_gateway.secure_egress_vpc1[0].id
   gw_size         = var.avx_gateway_size
   subnet          = aws_subnet.public_vpc1[count.index + 1].cidr_block
-}
-
-# Create an Aviatrix Standalone Gateways - Required for >2 AZs in 7.0
-resource "aviatrix_gateway" "secure_egress_vpc1" {
-  count        = var.deploy_avx_egress_gateways && var.number_of_azs > 2 ? var.number_of_azs : 0
-  cloud_type   = 1
-  account_name = var.aviatrix_aws_account_name
-  gw_name      = "avx-gateway-vpc1-${count.index}"
-  vpc_id       = aws_vpc.default[0].id
-  vpc_reg      = var.aws_region
-  gw_size      = var.avx_gateway_size
-  subnet       = aws_subnet.public_vpc1[count.index].cidr_block
-  single_ip_snat = var.enable_nat_avx_egress_gateways
-  depends_on = [
-    aws_route_table_association.public_vpc1
-  ]
 }
 
 # Aviatrix Statefull Firewall Policy - required for the demo to match FQDN and simultaneously enable Threatguard
@@ -68,7 +53,7 @@ resource "aviatrix_firewall_policy" "firewall_policy_1" {
 }
 
 
-# Create an Aviatrix Spoke/Egress Gateways in VPC2
+# Create an Aviatrix Spoke/Egress Gateway in VPC2
 resource "aviatrix_spoke_gateway" "secure_egress_vpc2" {
   count             = var.deploy_aws_tgw && var.deploy_avx_egress_gateways ? 1 : 0
   cloud_type        = 1
